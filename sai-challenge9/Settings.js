0e7064f898142c40f72567ed74c406fdf7b82364
Sai K <you@example.com>
2017-06-12 10:18:34 -0400

    Challenge 9 Native Routing

---
+++ sai-challenge9/Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <Text>Settings Page</Text>;
+  }
+}
