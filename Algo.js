ae74635c0518828babf009e30654ab4f737f41a7
Vineeth Gaddam <balabrahmamgaddam@balabrahmamsmbp.lan>
2017-06-20 09:59:21 -0400

    day 2 algos

--- Algo.js
+++ Algo.js
@@ -2,9 +2,8 @@ var prompt = require('prompt');
 
 prompt.start();
 
-prompt.get([Number], function (err, result) {
-
-    console.log(coinFlip(result.Number));
+prompt.get(['Min'], function (err, result) {
+    console.log(alarmClock(result.Min));
 });
 
 
@@ -36,3 +35,63 @@ function coinFlip(n) {
     }
     return ("heads " + heads + "  tails " + tails)
 }
+
+function tileCost(n) {
+
+    let width = 10
+    let height = 10
+
+    return (width * height) * n;
+}
+
+function change(cost, given) {
+    let totalCents = (given - cost) * 100;
+    let quarters = 0;
+    let dimes = 0;
+    let nickles = 0;
+    let pennies = 0;
+
+    quarters = Math.floor(totalCents / 25);
+    let leftovers = totalCents % 25;
+    dimes = Math.floor(leftovers / 10);
+    leftovers = leftovers % 10;
+    nickles = Math.floor(leftovers / 5);
+    pennies = Math.round(leftovers);
+    return "Quarters: " + quarters + " Dimes: " + dimes + " Nickles: " + nickles + " Pennies " + pennies;
+
+}
+
+function factorial(n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+function tempConverter(type, convertType, value) {
+    if (type == "farenheight") {
+        let celsius = Math.round((value - 32) * (5 / 9))
+        return celsius;
+    }
+    else {
+        let farenheight = Math.round((value * (9 / 5) + 32))
+        return farenheight;
+    }
+}
+
+function alarmClock(min) {
+    //does not work
+    //stack overflow, will fix tom
+    let totalSec = min * 60;
+    let now = new Date().getSeconds();
+    let target = now + totalSec;
+    while ((now <= target)) {
+        console.log(totalSec);
+        now = new Date().getSeconds()
+        totalSec = totalSec - 1;
+    }
+}
+
+
